dafb413502e687dd77fabd03e62d56a13a05f0c1
Rodion <rodion.kuskov@gmail.com>
2020-05-18 21:34:50 +0300

    Initial Commit

---
+++ BuyMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BuyMe
+//
+//  Created by Rodion Kuskov on 5/18/20.
+//  Copyright © 2020 Rodion Kuskov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
